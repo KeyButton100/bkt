39af3513c838fffe8878c2024846dddeae0f291d
KeyButton100 <darren.svp@gmail.com>
2026-02-21 14:17:02 +0700

    Add files via upload

--- bkt.py
+++ bkt.py
@@ -2,11 +2,21 @@ import pgzrun, random, pyautogui
 WIDTH, HEIGHT=pyautogui.size()
 TITLE= "bkt"
 
+mo=[]
 BKT=Actor("bucket.png")
+BKT.scale=0.5
 BKT.pos= WIDTH//2, HEIGHT-100
+element=["money", "banpeel", "coin"]
+def create_elements():
+    A=Actor(random.choice(element))
+    A.pos= random.randint(0, WIDTH-50), 0
+    mo.append(A)
 def draw():
-    screen.clear()
+    screen.blit("space.jpg", (0, 0))
     BKT.draw()
+    #screen.draw.rect(BKT.rect, "red")
+    for i in mo:
+        i.draw()
 
 def update():
     if keyboard.left:
@@ -17,4 +27,9 @@ def update():
         BKT.x=WIDTH
     elif BKT.x>WIDTH:
         BKT.x=0
+    for i in mo:
+        i.y+=10
+        if i.colliderect(BKT):
+            mo.remove(i)
+clock.schedule_interval(create_elements, 2)
 pgzrun.go()
